f60839d633a1175189c8a7eb0e9569784db11ae8
Premal upadhyay <34483490+premaluu@users.noreply.github.com>
2020-10-12 21:13:14 +0530

    test2

--- assignment 5/assignment5.php
+++ assignment 5/assignment5.php
@@ -16,7 +16,7 @@
             <form class="form-horizontal hr" method="post" action="insertData.php">
 			<h2>Registration Here</h2>
               <div  class="form-group">
-                <label class="control-label col-sm-4" for="fname">First Name:</label>                   
+                <label class="control-label col-sm-4" for="fname">First Name:</label>
                   <div class="col-sm-8">
                     <input type="text"   name="fname" placeholder="Enter Your First Name" class="form-controler" id="uname" >
                   </div>
@@ -25,14 +25,14 @@
               <div class="form-group">
                 <label class="control-label col-sm-4" for="mname">Middle Name:</label>
                  <div class="col-sm-8">
-                   <input type="text"   name="mname" placeholder="Enter Your Middle Name"class="form-controler" id="uname">
+                   <input type="text"   name="mname" placeholder="Enter Your Middle name"class="form-controler" id="uname">
                  </div>
               </div>
           <!------>
 		      <div class="form-group">
                 <label class="control-label col-sm-4" for="lname">Last Name:</label>
                  <div class="col-sm-8">
-                  <input type="text"  name="lname" placeholder="Enter Your Last Name" class="form-controler" id="uname">
+                  <input type="text"  name="lname" placeholder="Enter Your Last name" class="form-controler" id="uname">
                  </div>
               </div>
           <!----->
@@ -58,13 +58,13 @@
                       <title></title>
                     </head>
                     <body>
-                    
+
                     </body>
                     </html>
 					   Male:<input type="radio"  name="gen" class="form-controler" id="uname" value="m">
-					
+
 					  Female:<input type="radio"  name="gen" class="form-controler" id="uname" value="f">
-					  
+
 					  Others:<input type="radio"  name="gen" class="form-controler" id="uname" value="o">
                     </div>
                 </div>
@@ -75,7 +75,7 @@
  <input type="checkbox" name="Pro" value="student"" color="blue">Student</input><br>
   <input type="checkbox" name="Pro" value="teacher" color="black">Teacher</input><br>
   <input type="checkbox" name="Pro" value="employe" color="black">employe</input><br>
- 
+
  </div>
  </div>
     <!-----><br>
@@ -97,7 +97,7 @@ in the workshop?</label><br>
   </div><br>
       <!----->
 	  <div class="form-group">
-               
+
                  <div class="col-sm-offset-2col-sm-8">
                   <button  type="submit"class="btn btn-default">Submit</button>
                  </div>
@@ -109,7 +109,7 @@ in the workshop?</label><br>
            </div>
     </div>
   </div>
-    
+
 
 </body>
 </center>
